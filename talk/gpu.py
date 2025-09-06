281856fef967bc992b9967a2d8cd4a70f689e5b0
KoryB <byrnekory@gmail.com>
2025-09-06 15:14:18 -0400

    almost every case is working except one

--- talk/gpu.py
+++ talk/gpu.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 import numpy as np
 import pygame
+from numba import njit
 
-
-SURFACE_MODE = "RGBA"
-DEFAULT_COLOR = np.uint32(0xFF00FFFF)
-BYTE_WIDTH = 4
 
 @dataclass
 class Color:
@@ -17,6 +15,11 @@ class Color:
 
     def as_numpy(self) -> np.ndarray:
         return np.array([self.r, self.g, self.b, self.a], dtype=np.uint8)
+
+
+SURFACE_MODE = "RGBA"
+DEFAULT_COLOR = Color(255, 0, 255)
+BYTE_WIDTH = 4
 
 
 class Buffer:
@@ -40,7 +43,7 @@ def get_buffer(width: int, height: int, color: Color = Color(0xFF, 0x00, 0xFF)) 
 
 
 def clear(buff: Buffer, color: Color):
-    buff.array[:, :, :] = color.as_numpy()[:, np.newaxis, np.newaxis]
+    buff.array[:, :, :] = color.as_numpy()
 
 
 def draw_rect(buff: Buffer, x: int, y: int, w: int, h: int, color: Color):
@@ -54,9 +57,78 @@ def get_aabb(*points: np.ndarray):
     return min, max
 
 
-# https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates
-# Currently only accepts ints, operates directly on the buffer
-def draw_triangle(buff: Buffer, a: np.ndarray, b: np.ndarray, c: np.ndarray, color: Color):
+def get_grid(min: np.ndarray, max: np.ndarray) -> np.ndarray:
+    return np.mgrid[min[0]:max[0], min[1]:max[1]].reshape(2,-1).T
+
+
+def get_line(a: np.ndarray, b: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    npoints = np.max(np.array([np.abs(b[0] - a[0]) + 1, np.abs(b[1] - a[1]) + 1]))
+    x = np.linspace(a[0], b[0], npoints, dtype=np.int32)
+    y = np.linspace(a[1], b[1], npoints, dtype=np.int32)
+
+    return x, y
+
+
+def interpolate_y(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    npoints = b[0] - a[0]
+
+    return np.linspace(a[1], b[1], npoints, dtype=np.int32)
+
+
+def interpolate_x(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    npoints = b[1] - a[1]
+
+    return np.linspace(a[0], b[0], npoints, dtype=np.int32)
+
+
+
+def draw_line(array: np.ndarray, a: np.ndarray, b: np.ndarray, color: np.ndarray):
+    x, y = get_line(a, b)
+
+    array[y, x, :] = color
+
+
+@njit
+def triangle_blit(buff: np.ndarray, x_left: np.ndarray, x_right: np.ndarray, y0: np.int32, y1: np.int32, color: np.ndarray):
+    for y in range(y0, y1 + 1):
+        for x in range(x_left[y - y0], x_right[y - y0] + 1):
+            buff[y, x, :] = color
+
+
+def draw_triangle(buff: np.ndarray, a: np.ndarray, b: np.ndarray, c: np.ndarray, color: np.ndarray):
+    # Sort point y values
+    if b[1] < a[1]:
+        a, b = b, a
+
+    if c[1] < a[1]:
+        a, c = c, a
+
+    if c[1] < b[1]:
+        b, c = c, b
+
+    print(a, b, c, sep='\n')
+
+    xab = interpolate_x(a, b)
+    xbc = interpolate_x(b, c)
+    xac = interpolate_x(a, c)
+
+    xabc = np.concatenate([xab[:-1], xbc])
+
+    midpoint = np.floor_divide(xabc.size, 2, dtype=np.int32)
+
+    if xac[midpoint] < xabc[midpoint]:
+        x_left = xac
+        x_right = xabc
+
+    else:
+        x_left = xabc
+        x_right = xac
+
+    triangle_blit(buff, x_left, x_right, a[1], c[1], color)
+    
+
+
+def get_triangle_points_bary(a: np.ndarray, b: np.ndarray, c: np.ndarray) -> np.ndarray:
     # I'll need to do bounds detection
 
     aabb_min, aabb_max = get_aabb(a, b, c)
@@ -65,7 +137,7 @@ def draw_triangle(buff: Buffer, a: np.ndarray, b: np.ndarray, c: np.ndarray, col
         return
 
     # https://stackoverflow.com/questions/32208359/is-there-a-multi-dimensional-version-of-arange-linspace-in-numpy
-    points = np.mgrid[aabb_min[0]:aabb_max[0], aabb_min[1]:aabb_max[1]].reshape(2,-1).T
+    points = get_grid(aabb_min, aabb_max)
 
     v0 = b - a
     v1 = c - a
@@ -85,7 +157,13 @@ def draw_triangle(buff: Buffer, a: np.ndarray, b: np.ndarray, c: np.ndarray, col
 
     mask = (v > 0) & (w > 0) & (u > 0) & (v + w + u <= 1.0)
 
-    triangle_points = points[mask]
+    return points[mask]
 
-    buff.array[triangle_points[:, 1], triangle_points[:, 0], :] = color.as_numpy()[np.newaxis, np.newaxis, :]
+
+# https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates
+# Currently only accepts ints, operates directly on the buffer
+# def draw_triangle(buff: Buffer, a: np.ndarray, b: np.ndarray, c: np.ndarray, color: Color):
+#     triangle_points = get_triangle_points(a, b, c)
+
+#     buff.array[triangle_points[:, 1], triangle_points[:, 0], :] = color.as_numpy()[np.newaxis, np.newaxis, :]
 
--- talk/main.py
+++ talk/main.py
@@ -15,12 +15,15 @@ def parse_args():
 def main(args):
     pygame.init()
 
-    screen = pygame.display.set_mode((1280,720))
+    screen = pygame.display.set_mode((640, 360))
 
-    buff = gpu.get_buffer(540, 720)
+    buff = gpu.get_buffer(640, 360)
     
     gpu.draw_rect(buff, 16, 32, 64, 128, gpu.Color(0x00, 0x00, 0x00))
-    gpu.draw_triangle(buff, np.array([64, 64]), np.array([128, 128]), np.array([64, 128]), gpu.Color(255, 0, 0))
+
+    # for i in range(10_000):
+        # gpu.draw_line(buff.array, np.array([0, i//10], dtype=int), np.array([i//10, 0], dtype=int), gpu.Color(255, 0, 0).as_numpy())
+    # gpu.draw_triangle(buff.array, np.array([64, 64]), np.array([360, 128]), np.array([64, 128]), gpu.Color(255, 0, 0).as_numpy())
 
     square = buff.get_surface()
 
@@ -32,13 +35,20 @@ def main(args):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 raise SystemExit
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                gpu.clear(buff, gpu.DEFAULT_COLOR)
+
+                gpu.draw_triangle(buff.array, np.array([64, 64]), np.array(event.pos), np.array([64, 128]), gpu.Color(255, 0, 0).as_numpy())
 
         # Do logical updates here.
         # ...
 
+        
+
         screen.fill("purple")  # Fill the display with a solid color
 
-        screen.blit(square, ((1280-720)/2, 0))
+        screen.blit(square, (0, 0))
 
         # Render the graphics here.
         # ...
