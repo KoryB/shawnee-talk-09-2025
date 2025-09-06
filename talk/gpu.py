1ab545c818eb4e26206f573435397c29d9ae92b0
KoryB <byrnekory@gmail.com>
2025-09-06 19:37:10 -0400

    Got a temporary buffer system working, but python with is slow

--- talk/gpu.py
+++ talk/gpu.py
@@ -1,25 +1,54 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Union
+from contextlib import contextmanager
 
 import numpy as np
 import pygame
-from numba import njit, jit
+from numba import njit, prange
+from enum import Enum
 
 
-@dataclass
-class Color:
-    r: np.uint8
-    g: np.uint8
-    b: np.uint8
-    a: np.uint8 = 0xFF
-
-    def as_numpy(self) -> np.ndarray:
-        return np.array([self.r, self.g, self.b, self.a], dtype=np.uint8)
+"""
+Everything is assumed to be single threaded (yay)
+"""
 
 
 SURFACE_MODE = "RGBA"
+OUTPUT_WIDTH = 320
+OUTPUT_HEIGHT = 240
+
+INTEGER_DTYPE = np.int16
+UNSIGNED_INTEGER_DTYPE = np.uint16
+FLOAT_DTYPE = np.float32
+MAX_INT = np.iinfo(INTEGER_DTYPE).max
+MAX_UINT = np.iinfo(UNSIGNED_INTEGER_DTYPE).max
+NUM_SCRATCH_BUFFERS = 32
+SEQUENCE_INT = np.arange(MAX_INT, dtype=INTEGER_DTYPE)
+SEQUENCE_UINT = np.arange(MAX_UINT, dtype=UNSIGNED_INTEGER_DTYPE)
+
+SCRATCH_BUFFER_SIZE = MAX_UINT
+
+# Could optimize to work as an object pool type thing
+SCRATCH_BUFFER_INT = np.zeros((NUM_SCRATCH_BUFFERS, SCRATCH_BUFFER_SIZE), dtype=INTEGER_DTYPE)
+SCRATCH_BUFFER_UINT = np.zeros((NUM_SCRATCH_BUFFERS, SCRATCH_BUFFER_SIZE), dtype=UNSIGNED_INTEGER_DTYPE)
+SCRATCH_BUFFER_FLOAT = np.zeros((NUM_SCRATCH_BUFFERS, SCRATCH_BUFFER_SIZE), dtype=FLOAT_DTYPE)
+
+SCRATCH_BUFFER_INT_IN_USE = np.array([False] * NUM_SCRATCH_BUFFERS, dtype=bool)
+SCRATCH_BUFFER_UINT_IN_USE = np.array([False] * NUM_SCRATCH_BUFFERS, dtype=bool)
+SCRATCH_BUFFER_FLOAT_IN_USE = np.array([False] * NUM_SCRATCH_BUFFERS, dtype=bool)
+
+
+class Color:
+    def __init__(self, r: np.uint8, g: np.uint8, b: np.uint8, a: np.uint8 = 0xFF):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
+        self.array = np.array([r, g, b, a], dtype=np.uint8)
+
+
 DEFAULT_COLOR = Color(255, 0, 255)
-BYTE_WIDTH = 4
 
 
 class Buffer:
@@ -27,27 +56,70 @@ class Buffer:
         self.array = array
         self.mode = SURFACE_MODE
 
+
     def get_surface(self):
         h, w, _ = self.array.shape
 
         surf = pygame.image.frombuffer(self.array.data, (w, h), SURFACE_MODE)
 
         return surf
+    
+# Refactor to be reusable?
+@contextmanager
+def sb_int(size: UNSIGNED_INTEGER_DTYPE):
+    assert not np.all(SCRATCH_BUFFER_INT_IN_USE)
+
+    index = np.argmin(SCRATCH_BUFFER_INT_IN_USE)
+    SCRATCH_BUFFER_INT_IN_USE[index] = True
+
+    try:
+        yield SCRATCH_BUFFER_INT[index, :size]
+
+    finally:
+        SCRATCH_BUFFER_INT_IN_USE[index] = False
+
+
+@contextmanager
+def sb_uint(size: UNSIGNED_INTEGER_DTYPE):
+    assert not np.all(SCRATCH_BUFFER_UINT_IN_USE)
+
+    index = np.argmin(SCRATCH_BUFFER_UINT_IN_USE)
+    SCRATCH_BUFFER_UINT_IN_USE[index] = True
+
+    try:
+        yield SCRATCH_BUFFER_UINT[index, :size]
+
+    finally:
+        SCRATCH_BUFFER_UINT_IN_USE[index] = False
+
+
+@contextmanager
+def sb_float(size: UNSIGNED_INTEGER_DTYPE):
+    assert not np.all(SCRATCH_BUFFER_FLOAT_IN_USE)
+
+    index = np.argmin(SCRATCH_BUFFER_FLOAT_IN_USE)
+    SCRATCH_BUFFER_FLOAT_IN_USE[index] = True
+
+    try:
+        yield SCRATCH_BUFFER_FLOAT[index, :size]
+
+    finally:
+        SCRATCH_BUFFER_FLOAT_IN_USE[index] = False
 
 
 def get_buffer(width: int, height: int, color: Color = Color(0xFF, 0x00, 0xFF)) -> Buffer:
-    raw = np.tile(color.as_numpy(), (height, width, 1))
+    raw = np.tile(color.array, (height, width, 1))
     buff = Buffer(raw)
 
     return buff
 
 
 def clear(buff: Buffer, color: Color):
-    buff.array[:, :, :] = color.as_numpy()
+    buff.array[:, :, :] = color.array
 
 
 def draw_rect(buff: Buffer, x: int, y: int, w: int, h: int, color: Color):
-    buff.array[y:(y+h), x:(x+w), :] = color.as_numpy()[np.newaxis, np.newaxis, :]
+    buff.array[y:(y+h), x:(x+w), :] = color.array
 
 
 def get_aabb(*points: np.ndarray):
@@ -61,38 +133,60 @@ def get_grid(min: np.ndarray, max: np.ndarray) -> np.ndarray:
     return np.mgrid[min[0]:max[0], min[1]:max[1]].reshape(2,-1).T
 
 
-def get_line(a: np.ndarray, b: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    npoints = np.max(np.array([np.abs(b[0] - a[0]) + 1, np.abs(b[1] - a[1]) + 1]))
-    x = np.linspace(a[0], b[0], npoints, dtype=np.int32)
-    y = np.linspace(a[1], b[1], npoints, dtype=np.int32)
+def linspace_int(
+        a: INTEGER_DTYPE, 
+        b: INTEGER_DTYPE, 
+        npoints: UNSIGNED_INTEGER_DTYPE, 
+        out: Union[np.ndarray, None] = None
+    ) -> Union[np.ndarray, UNSIGNED_INTEGER_DTYPE]:
+    
+    if out is None:
+        return np.linspace(a, b, npoints, dtype=INTEGER_DTYPE)
+    
+    # assert out.size == npoints, temp is not None, a != b
 
-    return x, y
+    with sb_float(npoints) as buff:
+        np.divide(SEQUENCE_UINT[:npoints], npoints, out=buff, dtype=FLOAT_DTYPE)  # interpolation
+        buff *= (b - a)  # Scale
+        buff += a  # min value
+        
+        out[:npoints] = buff  # copy to output, this will truncate integers
+
+    return npoints
 
 
-def interpolate_y(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+def interpolate_y(a: np.ndarray, b: np.ndarray, out: np.ndarray) -> np.ndarray:
     npoints = b[0] - a[0] + 1
 
-    return np.linspace(a[1], b[1], npoints, dtype=np.int32)
+    return linspace_int(a[1], b[1], npoints, out=out)
 
 
-def interpolate_x(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+def interpolate_x(a: np.ndarray, b: np.ndarray, out: np.ndarray) -> np.ndarray:
     npoints = b[1] - a[1] + 1
 
-    return np.linspace(a[0], b[0], npoints, dtype=np.int32)
+    return linspace_int(a[0], b[0], npoints, out=out)
 
+
+def get_line(a: np.ndarray, b: np.ndarray, out_x: np.ndarray, out_y: np.ndarray):
+    npoints = np.max(np.array([np.abs(b[0] - a[0]) + 1, np.abs(b[1] - a[1]) + 1]))
+    linspace_int(a[0], b[0], npoints, out=out_x)
+    linspace_int(a[1], b[1], npoints, out=out_y)
+
+    return npoints
 
 
 def draw_line(array: np.ndarray, a: np.ndarray, b: np.ndarray, color: np.ndarray):
-    x, y = get_line(a, b)
+    with sb_int(SCRATCH_BUFFER_SIZE) as out_x, sb_int(SCRATCH_BUFFER_SIZE) as out_y:
+        npoints = get_line(a, b, out_x, out_y)
 
-    array[y, x, :] = color
+        array[out_y[:npoints], out_x[:npoints], :] = color
 
 
 @njit
-def triangle_blit(buff: np.ndarray, x_left: np.ndarray, x_right: np.ndarray, y0: np.int32, y1: np.int32, color: np.ndarray):
+def triangle_blit(buff: np.ndarray, x_left: np.ndarray, x_right: np.ndarray, y0: np.int16, y1: np.int16, color: np.ndarray):
     for y in range(y0, y1 + 1):
-        for x in range(x_left[y - y0], x_right[y - y0] + 1):
-            buff[y, x, :] = color
+        i = y - y0
+        buff[y, x_left[i]:(x_right[i]+1), :] = color
 
 
 def draw_triangle(buff: np.ndarray, a: np.ndarray, b: np.ndarray, c: np.ndarray, color: np.ndarray):
@@ -106,23 +200,30 @@ def draw_triangle(buff: np.ndarray, a: np.ndarray, b: np.ndarray, c: np.ndarray,
     if c[1] < b[1]:
         b, c = c, b
 
-    xab = interpolate_x(a, b)
-    xbc = interpolate_x(b, c)
-    xac = interpolate_x(a, c)
 
-    xabc = np.concatenate([xab[:-1], xbc])
+    with sb_int(SCRATCH_BUFFER_SIZE) as xab_full, sb_int(SCRATCH_BUFFER_SIZE) as xbc_full, sb_int(SCRATCH_BUFFER_SIZE) as xac_full, sb_int(SCRATCH_BUFFER_SIZE) as xabc_full:
+        npoints_xab = interpolate_x(a, b, out=xab_full)
+        npoints_xbc = interpolate_x(b, c, out=xbc_full)
+        npoints_xac = interpolate_x(a, c, out=xac_full)
 
-    midpoint = np.floor_divide(xabc.size, 2, dtype=np.int32)
+        xab = xab_full[:npoints_xab]
+        xbc = xbc_full[:npoints_xbc]
+        xac = xac_full[:npoints_xac]
+        xabc = xabc_full[:npoints_xac]
 
-    if xac[midpoint] < xabc[midpoint]:
-        x_left = xac
-        x_right = xabc
+        np.concatenate([xab[:-1], xbc], out=xabc)
 
-    else:
-        x_left = xabc
-        x_right = xac
+        midpoint = np.floor_divide(xabc.size, 2, dtype=UNSIGNED_INTEGER_DTYPE)
 
-    triangle_blit(buff, x_left, x_right, a[1], c[1], color)
+        if xac[midpoint] < xabc[midpoint]:
+            x_left = xac
+            x_right = xabc
+
+        else:
+            x_left = xabc
+            x_right = xac
+
+        triangle_blit(buff, x_left, x_right, a[1], c[1], color)
     
 
 
--- talk/main.py
+++ talk/main.py
@@ -15,9 +15,9 @@ def parse_args():
 def main(args):
     pygame.init()
 
-    screen = pygame.display.set_mode((640, 360))
+    screen = pygame.display.set_mode((640, 480))
 
-    buff = gpu.get_buffer(640, 360)
+    buff = gpu.get_buffer(320, 240)
     
     gpu.draw_rect(buff, 16, 32, 64, 128, gpu.Color(0x00, 0x00, 0x00))
 
@@ -25,7 +25,8 @@ def main(args):
         # gpu.draw_line(buff.array, np.array([0, i//10], dtype=int), np.array([i//10, 0], dtype=int), gpu.Color(255, 0, 0).as_numpy())
     # gpu.draw_triangle(buff.array, np.array([64, 64]), np.array([360, 128]), np.array([64, 128]), gpu.Color(255, 0, 0).as_numpy())
 
-    square = buff.get_surface()
+    buff_surface_raw = buff.get_surface()
+    buff_target = pygame.Surface((640, 480), flags=pygame.SRCALPHA)
     font = pygame.font.SysFont("Georgia", 16)
 
     clock = pygame.time.Clock()
@@ -40,8 +41,8 @@ def main(args):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 gpu.clear(buff, gpu.DEFAULT_COLOR)
 
-        for i in range(100):
-            gpu.draw_triangle(buff.array, np.array([0,0]), np.array([0, 120]), np.array([200, 120]), gpu.Color(255, 0, 0).as_numpy())
+        for i in range(1000):
+            gpu.draw_triangle(buff.array, np.array([0,0]), np.array([0, 120]), np.array([200, 120]), gpu.Color(255, 0, 0).array)
 
         # Do logical updates here.
         # ...
@@ -50,7 +51,8 @@ def main(args):
 
         screen.fill("purple")  # Fill the display with a solid color
 
-        screen.blit(square, (0, 0))
+        pygame.transform.scale2x(buff_surface_raw, buff_target)
+        screen.blit(buff_target, (0, 0))
         surf = font.render(f"FPS: {clock.get_fps()}", False, (0, 0, 0))
         screen.blit(surf, (0, 0))
 
